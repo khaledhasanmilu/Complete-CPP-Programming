63ad2a48d3cceda5ad1f1c27354654d5cec27e2a
Khaled Hasan Milu <118599823+khaledhasanmilu@users.noreply.github.com>
2024-09-23 12:01:18 +0600

    Create comment.cpp

---
+++ Comment/comment.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // this is single line comment.
+    /*
+        this is multiline comment
+        we write lot of note here in multiline.
+    */
+    cout << "Hello World!" << endl;
+
+    return 0;
+}
