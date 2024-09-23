7b09706c82c2021376d2c9dddbc1b8af38d0af6e
Khaled Hasan Milu <118599823+khaledhasanmilu@users.noreply.github.com>
2024-09-24 03:30:04 +0600

    Add files via upload

---
+++ Data Types/basicDataType.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int myNum = 5;             // Integer (whole number)
+    float myFloatNum = 5.99;   // Floating point number
+    double myDoubleNum = 9.98; // Floating point number
+    char myLetter = 'D';       // Character
+    bool myBoolean = true;     // Boolean
+    string myText = "Hello";   // String
+
+    return 0;
+}
