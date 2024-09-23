b53e05d6545f4d7f8829653b8ec13b758a9bf6ff
Khaled Hasan Milu <118599823+khaledhasanmilu@users.noreply.github.com>
2024-09-24 03:29:41 +0600

    Create numbers.cpp

---
+++ Data Types/numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 245;
+    float m = 23.33;
+    double d1 = 12E4;
+    cout << "Integer value: " << n << "\n"
+         << "Float Value: " << m << "\n"
+         << "Double valye: " << d1 << endl;
+
+    return 0;
+}
