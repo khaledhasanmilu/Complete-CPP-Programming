b0c78b9166e0823df57632681bc7db2648cb5b38
Khaled Hasan Milu <118599823+khaledhasanmilu@users.noreply.github.com>
2024-09-24 03:11:59 +0600

    Create input.cpp

---
+++ Input/input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // input part
+    int n;
+    cout << "Enter any number: ";
+    cin >> n;
+    cout << "You entered: " << n << endl;
+
+    return 0;
+}
