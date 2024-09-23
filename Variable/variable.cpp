7230002c4ab69911afc6a1ccc8a6a88144fd7c6a
Khaled Hasan Milu <118599823+khaledhasanmilu@users.noreply.github.com>
2024-09-24 03:10:14 +0600

    Create variable.cpp

---
+++ Variable/variable.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int myNum = 5;            // Integer (whole number without decimals)
+    double myFloatNum = 5.99; // Floating point number (with decimals)
+    char myLetter = 'D';      // Character
+    string myText = "Hello";  // String (text)
+    bool myBoolean = true;    // Boolean (true or false)
+
+    //print part for this variable
+    cout<<myNum<<"\n"<<myFloatNum<<"\n"<<myLetter<<"\n"<<myText<<"\n";
+
+    return 0;
+}
