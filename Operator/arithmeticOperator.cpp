1b9757453c049da7a4571e38f86e06e164362619
Khaled Hasan Milu <118599823+khaledhasanmilu@users.noreply.github.com>
2024-09-25 00:47:28 +0600

    Add files via upload

---
+++ Operator/arithmeticOperator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 15;
+    int b = 5;
+    int sum = a + b;
+    int substract = a - b;
+    int multiplication = a * b;
+    int division = a / b;
+    int reminder = a % b;
+    cout << "Sum: " << sum << "\n";
+    cout << "Substract: " << substract << "\n";
+    cout << "Multiplication:" << multiplication << "\n";
+    cout << "Division: " << division << "\n";
+    cout << "Reminder: " << reminder << "\n";
+    return 0;
+}
