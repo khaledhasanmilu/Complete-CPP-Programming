b430cbf90f1a13a48e80b1a72d24a53750c649f6
Khaled Hasan Milu <118599823+khaledhasanmilu@users.noreply.github.com>
2024-09-27 12:46:11 +0600

    Update LeapYear.cpp

--- Control Flow/LeapYear.cpp
+++ Control Flow/LeapYear.cpp
@@ -5,9 +5,7 @@ int main() {
     int year;
     cout << "Enter a year: ";
     cin >> year;
-
-    // Simplified condition for checking leap year
-    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+      if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
         cout << year << " is a leap year." << endl;
     } else {
         cout << year << " is not a leap year." << endl;
