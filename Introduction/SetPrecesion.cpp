10dd8b0e23333418c557d04d190ea8c21ec36ac3
Khaled Hasan Milu <118599823+khaledhasanmilu@users.noreply.github.com>
2024-11-17 10:06:13 +0600

    Add files via upload

---
+++ Introduction/SetPrecesion.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    double x = 23.65436;
+    cout << fixed << setprecision(2) << x << endl;
+    return 0;
+}
---
+++ Introduction/ifElse.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int x = 20;
+  x%2==0 ? cout<< " Even\n" : cout<<"Odd\n";
+ return 0;
+}
