53d231f8f514d31f10e1b428850f53ad57a94312
Khaled Hasan Milu <118599823+khaledhasanmilu@users.noreply.github.com>
2024-11-28 22:49:13 +0600

    Add files via upload

---
+++ String/StringConstructor.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   //string s = "Hello";
+   //string s("Hello");
+   //string s("Hello World!",7);
+//    string s = "Hello World!";
+//    string t(s,4); //falay dibe 4 er age porjnto sob kichu ke and bakigula print hbe.
+string t(4,'A');
+   cout<< t <<endl;
+ return 0;
+}
